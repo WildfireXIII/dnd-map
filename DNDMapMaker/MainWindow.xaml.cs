d1b2aef45f3489975ab7258433c700e1a7da9ddd
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-02-03 21:56:01 -0600

    Initial commit

---
+++ DNDMapMaker/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DNDMapMaker
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			if (Master.DISPLAY_DEBUG) { lblDebug.IsEnabled = true; lblDebug.Visibility = Visibility.Visible; }
+
+			// get map window
+			MapDesignWindow mapWin = new MapDesignWindow();
+			mapWin.Show();
+
+			log("Log initialized!");
+		}
+
+		// FUNCTIONS
+
+		public void log(string msg) { lblDebug.Content += msg + "\n"; }
+		public void clearLog() { lblDebug.Content = ""; }
+		
+
+		// EVENT HANDLERS
+	}
+}
---
+++ DNDMapMaker/MapDesignWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNDMapMaker
+{
+
+
+	public partial class MapDesignWindow : Window
+	{
+		public MapDesignWindow()
+		{
+			InitializeComponent();
+
+			if (Master.DISPLAY_DEBUG) { lblDebug.IsEnabled = true; lblDebug.Visibility = Visibility.Visible; }
+
+			log("Log initialized!");
+		}
+
+		// FUNCTIONS
+
+		public void log(string msg) { lblDebug.Content += msg + "\n"; }
+		public void clearLog() { lblDebug.Content = ""; }
+
+		// EVENT HANDLERS
+	}
+}
